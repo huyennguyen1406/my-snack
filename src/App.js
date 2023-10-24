fe35e2d1ef57396bbdcc33c3be2d1890212a12f6
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-24 14:41:55 +0700

    use prop child

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { initialTravelPlan } from "./places.js";
 
-function App() {
+// const intialItems = [
+//   { title: 'pretzels', id: 0 },
+//   { title: 'crispy seaweed', id: 1 },
+//   { title: 'granola bar', id: 2 },
+// ];
+
+// export default function App() {
+//   const [items, setItems] = useState(intialItems);
+//   const [selectedId, setSelectedId] = useState(0);
+
+//   const selectedItem = items.find(item => 
+//     item.id === selectedId
+//     );
+
+//   function handleItemChange(id, e) {
+//     setItems( items.map(item => {
+//       if (item.id === id) {
+//         return {
+//           ...item,
+//           title: e.target.value,
+//         };
+//       } else {
+//         return item;
+//       }
+//     }));
+//   }
+
+//   return (
+//     <>
+//     <h2>What's your travel snack?</h2>
+//     <ul>
+//       {items.map((item, index) => (
+//         <li key={item.id}>
+//           <input
+//           value={item.title}
+//           onChange={e => {
+//             handleItemChange(item.id, e)
+//           }}
+//           />
+//           {' '}
+//           <button onClick={() => {
+//             setSelectedId(item.id);
+//           }}>
+//             Choose</button>
+//         </li>
+//       ))}
+//     </ul>
+//     <p>You picked {selectedItem.title}.</p>
+//     </>
+//   )
+// }
+
+function PlaceTree({ place }) {
+  const childPlaces = place.childPlaces;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>
+      {place.title}
+      {childPlaces.length > 0 && (
+        <ol>
+          {childPlaces.map(place => (
+            <PlaceTree key={place.id} place={place} />
+          ))}
+        </ol>
+      )}
+    </li>
   );
 }
 
-export default App;
+export default function TravelPlan() {
+  const [plan, setPlan] = useState(initialTravelPlan);
+  const plantes = plan.childPlaces;
+  return (
+    <>
+    <h2>Places to visit</h2>
+    <ol>
+      {plantes.map(place => (
+        <PlaceTree key={place.id} place={place} />
+      ))}
+    </ol>
+    </>
+  );
+}
