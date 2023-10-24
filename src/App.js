be480f9546e0f638f04fe4b414c167295d47b50d
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-24 16:16:30 +0700

    update states

--- src/App.js
+++ src/App.js
@@ -53,43 +53,68 @@ import { initialTravelPlan } from "./places.js";
 //   )
 // }
 
-function PlaceTree({ id, placesById }) {
+
+export default function TravelPlan() {
+  const [plan, setPlan] = useState(initialTravelPlan);
+
+  function handleComplete(parentId, childId) {
+    const parent = plan[parentId];
+
+    const nextParent = {
+      ...parent,
+      childIds: parent.childIds
+        .filter(id => id !== childId)
+    };
+    setPlan({
+      ...plan,
+      [parentId]: nextParent
+    });
+  }
+
+  const root = plan[0];
+  const planetIds = root.childIds;
+  return (
+    <>
+      <h2>Places to visit</h2>
+      <ol>
+        {planetIds.map(id => (
+          <PlaceTree
+            key={id}
+            id={id}
+            parentId={0}
+            placesById={plan}
+            onComplete={handleComplete}
+          />
+        ))}
+      </ol>
+    </>
+  );
+}
+
+function PlaceTree({ id, parentId, placesById, onComplete }) {
   const place = placesById[id];
   const childIds = place.childIds;
   return (
     <li>
       {place.title}
-      {childIds.length > 0 && (
+      <button onClick={() => {
+        onComplete(parentId, id);
+      }}>
+        Complete
+      </button>
+      {childIds.length > 0 &&
         <ol>
           {childIds.map(childId => (
-            <PlaceTree 
-            key={childId} 
-            id={childId}
-            placesById={placesById} 
+            <PlaceTree
+              key={childId}
+              id={childId}
+              parentId={id}
+              placesById={placesById}
+              onComplete={onComplete}
             />
           ))}
         </ol>
-      )}
+      }
     </li>
-  );
-}
-
-export default function TravelPlan() {
-  const [plan, setPlan] = useState(initialTravelPlan);
-  const root = plan[0];
-  const planteIds = root.childIds;
-  return (
-    <>
-    <h2>Places to visit</h2>
-    <ol>
-      {planteIds.map(id => (
-        <PlaceTree 
-        key={id} 
-        id={id}
-        placesById={plan} 
-        />
-      ))}
-    </ol>
-    </>
   );
 }
